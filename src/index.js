417311feec98f20d9a63d77e83cfefa95353d732
Pranjal705 <62437656+Pranjal705@users.noreply.github.com>
2020-06-16 09:29:30 +0530

    Organized the folders, created error-boundary

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
+// import Card from './Card.js';
+import App from './containers/App'
+import 'tachyons';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+  <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
